9a6d2d618f6ea35eb1638fd42285e5c3e2b6e8cd
cklsh <siti.khalishah@yahoo.com>
2018-12-18 18:20:56 +0700

    convert to rails

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,4 @@
 # frozen_string_literal: true
-
 class ProductsController < ApplicationController
     def index; end
     def show; end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
+  get 'home/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  root :to => "homes#index"
-
-  resources :products, only:  %i[index show]
-
-  get '/contact_us', to:'contacts#contact_us'
-  get '/about_us', to: 'about#about_us'
+  resources :products
+  root 'home#index'
 end
